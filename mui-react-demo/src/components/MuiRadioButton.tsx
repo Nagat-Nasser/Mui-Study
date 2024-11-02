0e57119c6cae09b28ee482a9dbc1fe4b088ac9a1
Nagat Nasser Abd al-Tawwab <109410503+Nagat-Nasser@users.noreply.github.com>
2024-11-02 21:05:09 +0200

    Create RadioGroupButtons

--- mui-react-demo/src/components/MuiRadioButton.tsx
+++ mui-react-demo/src/components/MuiRadioButton.tsx
@@ -1,10 +1,23 @@
-import { Box } from "@mui/material"
+import { Box, FormControl, FormControlLabel, FormHelperText, FormLabel, Radio, RadioGroup } from "@mui/material";
+import { type SetStateAction, useState } from "react";
 const MuiRadioButton = () => {
-  return (
-    <Box>
+  const [RadioButtonValue, setRadioButtonValue] = useState('')
+  const handleRadioButtonChange=(e: { target: { value: SetStateAction<string>; }; })=>{
+    setRadioButtonValue(e.target.value)
+  }
+	return (
+		<Box>
+			<FormControl error>
+				<FormLabel>Years of experience</FormLabel>
+      <RadioGroup  row  value={RadioButtonValue} onChange={handleRadioButtonChange} >
+        <FormControlLabel  control={<Radio color="warning" size="small"/>} label='0-2' value='0-2'/>
+        <FormControlLabel  control={<Radio/>} label='3-5' value='3-5' />
+        <FormControlLabel  control={<Radio/>} label='6-10' value='6-10'/>
+      </RadioGroup>
+      <FormHelperText>Invalid Selection</FormHelperText>
+			</FormControl>
+		</Box>
+	);
+};
 
-    </Box>
-  )
-}
-
-export default MuiRadioButton 
+export default MuiRadioButton;
